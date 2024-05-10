08ac664522119a65c5a54d8360cad033ba59ac53
ekokocat <eleanorkokoska@gmail.com>
2024-05-10 11:02:03 -0400

    fixed student.java

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -5,9 +5,11 @@ public class PlannerRunner {
 
         // Create a new student
         Student jada = new Student();
+        Student jack = new Student(); 
 
         // Gets the student's choice until the student exits
         jada.getChoice();
+        jack.getChoice(); 
 
     }
 }
